04e26e4ef21d93bf1047442d7c7a0ccdc29512ce
Viktor Ens <v.ens86@yahoo.de>
2024-10-05 19:48:35 +0200

    refactor machineParkService

--- src/app/services/machine-park.service.ts
+++ src/app/services/machine-park.service.ts
@@ -1,19 +1,22 @@
-import { Injectable, OnDestroy } from '@angular/core';
+import { inject, Injectable, OnDestroy } from '@angular/core';
 import { Machine } from '../interfaces/machine';
 import { FirebaseService } from './firebase.service';
 import { Subscription } from 'rxjs';
+import { SnackbarService } from './snackbar.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MachineParkService implements OnDestroy{
 
-  machineList: any[] = [];
+  private firebase = inject(FirebaseService);
+  private snackbarService = inject(SnackbarService);
 
+  public machineList: any[] = [];
   private machineListSubscription: Subscription;
   public machineTypes: string[] = ['mill', 'turn', 'millturn']
 
-  constructor(private firebase: FirebaseService) { 
+  constructor() { 
     
     this.machineListSubscription = this.firebase.machinesList$.subscribe((machines) => {
       this.machineList = machines;
@@ -30,21 +33,42 @@ export class MachineParkService implements OnDestroy{
   }
 
   addMachine(machine: Machine) {
+    try{
     this.firebase.addMachines(machine);
     this.machineList.push(machine)
+    this.snackbarService.openSnackBar('Machine added', 'success-snackbar')
+    } catch (error) {
+      console.error(error)
+      this.snackbarService.openSnackBar('The machine could not be added. Please check your connection and try again', 'error-snackbar')
+    }
   }
 
   deleteMachine(docId: string) {
-    console.log(docId);
-    
-    this.firebase.deleteMachine(docId);
+    try {
+      this.firebase.deleteMachine(docId);
+      this.snackbarService.openSnackBar('Machine deleted successfully', 'success-snackbar')
+    } catch (error) {
+      console.error(error)
+      this.snackbarService.openSnackBar('The machine could not be deleted. Please check your connection and try again', 'error-snackbar')
+    }
   }
 
   updateMachine(machineId: string, machine: Machine) {
-    this.firebase.updateMachine(machineId, machine);
+    try {
+      this.firebase.updateMachine(machineId, machine);
+      this.snackbarService.openSnackBar('Machine updated', 'custom-snackbar')
+    } catch (error) {
+      console.error(error)
+      this.snackbarService.openSnackBar('The machine could not be updated. Please check your connection and try again', 'error-snackbar')
+    }
   }
 
   findMachineById(docId: string) {
-    return this.firebase.getSingleDocRef('machines', docId);
+    try {
+      return this.firebase.getSingleDocRef('machines', docId);
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
   }
 }
